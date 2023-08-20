013b35fbd3aa4ff8d76546356e1cd6ef6a3779a5
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-20 13:53:21 +0300

    frontend/feat/1/Impl Status Bar And Menu Bar: Separating the log logic into methods

--- models/log.py
+++ models/log.py
@@ -4,13 +4,17 @@ from PyQt5.QtWidgets import QPlainTextEdit, QWidget, QDesktopWidget
 class LogWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.init_ui()
+        self.set_initial_style()
+        self.adjust_size()
 
+    def init_ui(self):
         self.log = QPlainTextEdit(self)
         self.log.setReadOnly(True)
 
-        # Set gray background color
+    def set_initial_style(self):
         self.setStyleSheet("background-color: #CCCCCC;")
 
-        # Get screen dimensions
+    def adjust_size(self):
         screen = QDesktopWidget().screenGeometry()
         self.log.setFixedSize(screen.width(), 100)
