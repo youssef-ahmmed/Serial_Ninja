c8e745c7bbd42708c5003697986b16b469eff669
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-08-20 13:53:01 +0300

    frontend/feat/1/Impl Status Bar And Menu Bar: Seprataing the menu logic into methods

--- models/menu_bar.py
+++ models/menu_bar.py
@@ -4,26 +4,26 @@ from PyQt5.QtWidgets import QMenuBar, QMenu, QAction, QActionGroup
 class MenuBar(QMenuBar):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.init_menus()
+        self.init_actions()
+        self.init_encryption_menu()
 
-        # Main menus
+    def init_menus(self):
         self.file_menu = self.addMenu("File")
         self.settings_menu = self.addMenu("Settings")
         self.help_menu = self.addMenu("Help")
 
-        # Actions for the menu items
+    def init_actions(self):
         self.open_action = QAction("Open", self)
         self.save_action = QAction("Save", self)
         self.port_configurations_action = QAction("Port Configurations", self)
 
-        # Add actions to File menu
         self.file_menu.addAction(self.open_action)
         self.file_menu.addAction(self.save_action)
-
-        # Add actions to Settings menu
         self.settings_menu.addAction(self.port_configurations_action)
 
-        # Submenu for Encryption Mode
-        self.encryption_submenu = QMenu("Encryption Mode", self)
+    def init_encryption_menu(self):
+        self.encryption_mode_submenu = QMenu("Encryption Mode", self)
         self.encryption_action_group = QActionGroup(self)
 
         self.encryption_action = QAction("Encryption", self)
@@ -35,7 +35,7 @@ class MenuBar(QMenuBar):
         self.encryption_action_group.addAction(self.encryption_action)
         self.encryption_action_group.addAction(self.no_encryption_action)
 
-        self.encryption_submenu.addAction(self.encryption_action)
-        self.encryption_submenu.addAction(self.no_encryption_action)
+        self.encryption_mode_submenu.addAction(self.encryption_action)
+        self.encryption_mode_submenu.addAction(self.no_encryption_action)
 
-        self.settings_menu.addMenu(self.encryption_submenu)
+        self.settings_menu.addMenu(self.encryption_mode_submenu)
