b7afc5ce07ce93c085101e6254dd6d06584e6e87
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-08-23 14:08:09 +0300

    update setting dialog and main widget

--- models/main_widget.py
+++ models/main_widget.py
@@ -3,6 +3,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QDesktopWidget
 
 from button import RoundButton
+from control.button_controller import ButtonController
+from control.log_controller import LogController
+from control.password_controller import PasswordController
 from log import LogWidget
 from menu_bar import MenuBar
 from password import PasswordEntry
@@ -11,10 +14,9 @@ from password import PasswordEntry
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Custom Widgets Example")
-        screen = QDesktopWidget().screenGeometry()
+        self.setWindowTitle("Serial Ninja")
 
-        self.setGeometry(screen.left(), screen.top(), screen.width(), screen.height())
+        self.setGeometry(200, 200, 1000, 700)
 
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
@@ -25,9 +27,15 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout(self.central_widget)
 
         menu_bar = MenuBar(self)
+
         password_entry = PasswordEntry(self)
+        PasswordController(password_entry)
+
         round_button = RoundButton("Click Me!")
+        ButtonController(round_button)
+
         log_widget = LogWidget(self)
+        LogController(log_widget)
 
         layout.addWidget(menu_bar)
         layout.addWidget(password_entry)
--- models/setting_dialog.py
+++ models/setting_dialog.py
@@ -1,18 +1,14 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QLabel, QComboBox, \
-    QPushButton, QVBoxLayout, QFormLayout, QWidget, QHBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QDialog, QLabel, QComboBox, \
+    QPushButton, QVBoxLayout, QFormLayout, QWidget, QHBoxLayout, QSizePolicy, QLineEdit, QMessageBox
 
-BAUD_RATE = ['110', '300', '600', '1200', '2400', '4800', '9600', '14400',
-             '19200', '38400', '57600', '115200', '128000', '256000']
-
-COMMUNICATION_PROTOCOLS = ["Select Protocol", "UART", "SPI", "I2C", "CAN"]
-
-FLOW_CONTROL = ["None", "Xon/Xoff", "RTS/CTS", "DSR/DTR"]
+import strings
 
 
 class SerialSettingsDialog(QDialog):
     def __init__(self):
         super().__init__()
+
+        self.port_configurations = []
 
         self.init_ui()
 
@@ -21,7 +17,6 @@ class SerialSettingsDialog(QDialog):
         self.setGeometry(100, 100, 400, 300)
 
         self.create_widgets()
-        self.start_communication()
         self.create_layout()
 
     def create_widgets(self):
@@ -32,7 +27,7 @@ class SerialSettingsDialog(QDialog):
         self.protocol_layout = QHBoxLayout()
         self.protocol_label = QLabel("Communication Protocol:")
         self.protocol_combo = QComboBox()
-        self.protocol_combo.addItems(COMMUNICATION_PROTOCOLS)
+        self.protocol_combo.addItems(strings.COMMUNICATION_PROTOCOLS)
         self.protocol_combo.currentIndexChanged.connect(self.update_settings)
 
         self.protocol_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
@@ -44,38 +39,46 @@ class SerialSettingsDialog(QDialog):
         self.cancel_button = QPushButton("Cancel")
         self.save_button = QPushButton("Save")
 
-    def start_communication(self):
-        self.cancel_button.clicked.connect(self.reject)
-        self.save_button.clicked.connect(self.save_settings)
-
     def create_uart_settings(self):
         self.clear_layout(self.form_layout)
 
-        self.baudrate_label = QLabel("Baudrate:")
+        self.port_number_label = QLabel("Port Number")
+        self.port_edit = QLineEdit()
+        self.port_edit.setPlaceholderText("Enter port name")
+        self.port_edit.textChanged.connect(self.clear_error_style)
+
+        self.baudrate_label = QLabel("Baudrate")
         self.baudrate_combo = QComboBox()
-        self.baudrate_combo.addItems(BAUD_RATE)
+        self.baudrate_combo.addItems(strings.BAUD_RATE)
         self.baudrate_combo.setEditable(True)
 
-        self.data_bits_label = QLabel("Data Bits:")
+        self.data_bits_label = QLabel("Data Bits")
         self.data_bits_combo = QComboBox()
         self.data_bits_combo.addItems(["8", "7", "6", "5"])
 
-        self.parity_label = QLabel("Parity:")
+        self.parity_label = QLabel("Parity")
         self.parity_combo = QComboBox()
-        self.parity_combo.addItems(["None", "Even", "Odd"])
+        self.parity_combo.addItems(["N", "E", "O"])
 
-        self.stop_bits_label = QLabel("Stop Bits:")
+        self.stop_bits_label = QLabel("Stop Bits")
         self.stop_bits_combo = QComboBox()
         self.stop_bits_combo.addItems(["1", "2"])
 
-        self.flow_control_label = QLabel("Flow Control:")
+        self.flow_control_label = QLabel("Flow Control")
         self.flow_control_combo = QComboBox()
-        self.flow_control_combo.addItems(FLOW_CONTROL)
+        self.flow_control_combo.addItems(strings.FLOW_CONTROL)
 
+        self.time_out_label = QLabel("Timeout")
+        self.time_out_edit = QLineEdit()
+        self.time_out_edit.setPlaceholderText("Enter timeout")
+        self.time_out_edit.textChanged.connect(self.clear_error_style)
+
+        self.form_layout.addRow(self.port_number_label, self.port_edit)
         self.form_layout.addRow(self.baudrate_label, self.baudrate_combo)
         self.form_layout.addRow(self.data_bits_label, self.data_bits_combo)
         self.form_layout.addRow(self.parity_label, self.parity_combo)
         self.form_layout.addRow(self.stop_bits_label, self.stop_bits_combo)
+        self.form_layout.addRow(self.time_out_label, self.time_out_edit)
         self.form_layout.addRow(self.flow_control_label, self.flow_control_combo)
 
         self.settings_widget.show()
@@ -109,13 +112,22 @@ class SerialSettingsDialog(QDialog):
             if child.widget():
                 child.widget().deleteLater()
 
-    def save_settings(self):
-        selected_protocol = self.protocol_combo.currentText()
+    def show_protocol_selection_error(self):
+        self.error_dialog = QMessageBox()
+        self.error_dialog.setIcon(QMessageBox.Warning)
+        self.error_dialog.setWindowTitle("Error")
+        self.error_dialog.setText("Please select a communication protocol")
+        self.error_dialog.setStandardButtons(QMessageBox.Ok)
+        self.error_dialog.exec_()
 
-        if selected_protocol == "UART":
-            baudrate = self.baudrate_combo.currentText()
-            data_bits = self.data_bits_combo.currentText()
-            parity = self.parity_combo.currentText()
-            stop_bits = self.stop_bits_combo.currentText()
+    def set_error_style_port(self):
+        error_style = "border: 1px solid red;"
+        self.port_edit.setStyleSheet(error_style)
 
-        self.accept()
+    def set_error_style_time_out(self):
+        error_style = "border: 1px solid red;"
+        self.time_out_edit.setStyleSheet(error_style)
+
+    def clear_error_style(self):
+        self.port_edit.setStyleSheet("")
+        self.time_out_edit.setStyleSheet("")
