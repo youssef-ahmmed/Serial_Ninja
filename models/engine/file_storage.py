58ee89a83ae0b270c26f8c3a31e77a6ac30918a0
youssef-ahmmed <115221628+youssef-ahmmed@users.noreply.github.com>
2023-08-23 14:19:35 +0300

    Implement file storage that stores logging

---
+++ models/engine/file_storage.py
@@ -0,0 +1,23 @@
+import os
+from datetime import datetime
+import json
+
+
+class FileStorage:
+
+    def __init__(self):
+        self.__time_stamp = datetime.now().strftime("%m-%dT%H-%M")
+        self.__object = {}
+        self.list_of_log = []
+        self.__file_path = os.path.join('Logging/', f"file_at_{self.__time_stamp}.json")
+
+        if not os.path.isdir('Logging'):
+            os.mkdir("Logging")
+
+    def new(self, log_dict):
+        self.__object = log_dict.copy()
+
+    def save(self):
+        with open(self.__file_path, "w", encoding="UTF-8") as f:
+            self.list_of_log.append(self.__object)
+            json.dump(self.list_of_log, f, indent=4)
